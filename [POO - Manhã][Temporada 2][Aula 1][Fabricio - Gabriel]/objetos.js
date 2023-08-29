26f0d66f089ff540cf775be45c74155d600922f7
MelrickDavison <115827008+MelrickDavison@users.noreply.github.com>
2023-08-29 09:31:02 -0300

    add file

--- [POO - Manhã][Temporada 2][Aula 1][Fabricio - Gabriel]/objetos.js
+++ [POO - Manhã][Temporada 2][Aula 1][Fabricio - Gabriel]/objetos.js
@@ -1,6 +1,6 @@
 let aluno1 = new Aluno('2°(Manhã)', 'fagc1@aluno.ifal.edu.br','https://lh3.googleusercontent.com/d/17jAtucgnXEUVT06_Rhm-n1xQazWmsuM5', '2022304463', 'Fabricio Arcanjo Guimarães Costa', '16');
 let aluno2 = new Aluno('2°(Manhã)', 'gllf1@aluno.ifal.edu.br','https://lh3.googleusercontent.com/d/1NTR45eSWi_6haSJi3P-IMqGJdkrOt-0J', '20223056300', 'Gabriel Luiz Lourenço Farias', '17');
-let aluno3 = new Aluno('2°(Manhã)','mdrf1@aluno.ifal.edu.br','https://lh3.googleusercontent.com/d/1XUZw8EtjRqL7ouVNORZIVGii2FJxxL_F', '2022305307', 'Melrick', '16');
+let aluno3 = new Aluno('2°(Manhã)','mdrf1@aluno.ifal.edu.br','https://lh3.googleusercontent.com/d/1XUZw8EtjRqL7ouVNORZIVGii2FJxxL_F', '2022305307', 'Melrick Davison Fonseca Ribeiro', '16');
 
 let listaDeAlunos = [];
 listaDeAlunos.push(aluno1, aluno2, aluno3);
