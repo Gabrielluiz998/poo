b7f0c08819b04a18aeb85ee43f9b0ac1cc612bfc
MelrickDavison <115827008+MelrickDavison@users.noreply.github.com>
2023-09-04 10:34:12 -0300

    adicionando mais alunos

--- [POO - Manhã][Temporada 2][Aula 1][Fabricio - Gabriel]/objetos.js
+++ [POO - Manhã][Temporada 2][Aula 1][Fabricio - Gabriel]/objetos.js
@@ -1,6 +1,14 @@
 let aluno1 = new Aluno('2°(Manhã)', 'fagc1@aluno.ifal.edu.br','https://lh3.googleusercontent.com/d/17jAtucgnXEUVT06_Rhm-n1xQazWmsuM5', '2022304463', 'Fabricio Arcanjo Guimarães Costa', '16');
 let aluno2 = new Aluno('2°(Manhã)', 'gllf1@aluno.ifal.edu.br','https://lh3.googleusercontent.com/d/1NTR45eSWi_6haSJi3P-IMqGJdkrOt-0J', '20223056300', 'Gabriel Luiz Lourenço Farias', '17');
-let aluno3 = new Aluno('2°(Manhã)','mdrf1@aluno.ifal.edu.br','https://lh3.googleusercontent.com/d/1XUZw8EtjRqL7ouVNORZIVGii2FJxxL_F', '2022305307', 'Melrick Davison Fonseca Ribeiro', '16');
+let aluno3 = new Aluno('2°(Manhã)','mdrf1@aluno.ifal.edu.br','https://lh3.googleusercontent.com/d/1XUZw8EtjRqL7ouVNORZIVGii2FJxxL_F', '2022305307', 'Melrick Dison Fonseca Ribeiro', '16');
+let aluno4 = new Aluno('2°(Manhã)','kggr1@aluno.ifal.edu.br','https://lh3.googleusercontent.com/d/1uWL4Z7JRW478UM_pObd4dGNp--USG5or/', '2022304525', 'Kemylly Gabrielly Gomes Rodrigues', '17');
+let aluno5 = new Aluno('2°(Manhã)','gfdg1@aluno.ifal.edu.br','https://lh3.googleusercontent.com/d/1D0tK367ziUWFUs2PgIVZKbTHEuiPV43r/', '2022309216', 'Girleide', '17');
+let aluno6 = new Aluno('2°(Manhã)','trfm1@aluno.ifal.edu.br','https://lh3.googleusercontent.com/d/1yK1BWGSRxuHt4gP6mmYNOIRHs4plJ8gR/', '2022398620', 'Thiago', '16');
+let aluno7 = new Aluno('2°(Manhã)','ejcp1@aluno.ifal.edu.br','https://lh3.googleusercontent.com/d/1xkGio1rSh25XTFnKAHcwXXsFliwU22HQ/', '2022337845', 'Ellen Jaiany', '16');
+let aluno8 = new Aluno('2°(Manhã)','rjlsd1@aluno.ifal.edu.br','https://lh3.googleusercontent.com/d/1drscdtgDQC-LFQVWkxioz8aJAPlELlcP/', '202225625', 'Robert', '19');
+let aluno9 = new Aluno('2°(Manhã)','rcpz1@aluno.ifal.edu.br','https://lh3.googleusercontent.com/d/1vw8sfjAXTlZpfwTJNJSXJQd1sOgKiVb0/', '20223045652', 'Rebeca', '16');
+let aluno10 = new Aluno('2°(Manhã)','wmbs1@aluno.ifal.edu.br','https://lh3.googleusercontent.com/d/1veLv6ByeW7wDjrojoeoYjeE-vIprjSLz/', '202230512', 'Wytalo', '17');
+
 
 let listaDeAlunos = [];
 listaDeAlunos.push(aluno1, aluno2, aluno3);
